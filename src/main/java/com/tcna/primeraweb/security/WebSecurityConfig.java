37e241ff5f1fddd719549c8cbb2203e77450f9d2
Your Name <you@example.com>
2025-04-28 22:19:07 -0500

    Seguridad UserDetails

--- src/main/java/com/tcna/primeraweb/security/WebSecurityConfig.java
+++ src/main/java/com/tcna/primeraweb/security/WebSecurityConfig.java
@@ -1,33 +1,54 @@
 package com.tcna.primeraweb.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.tcna.primeraweb.services.impl.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration //esta anotación registra Beans en el contenedor de Spring Boot
 @EnableWebSecurity //habilitar la seguridad en la app y poder usar la BD sin problemas
 public class WebSecurityConfig {
 
-    @Autowired
-    private DataSource dataSource;
-
-    //AUTENTIFICACION con una BD
-    @Autowired
-    public void configAuthentication(AuthenticationManagerBuilder builder) throws Exception {
-        builder.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
-                .dataSource(dataSource)
-                .usersByUsernameQuery("select username, password, enabled from users where username=?")
-                .authoritiesByUsernameQuery("select username, role from users where username=?");
+    //Para tener al usuario en la fabrica de Spring
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsServiceImpl();
     }
 
-    //CREAMOS un FILTRO
+    //Para encriptar la contraseña del ususario
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    /*Haremos una implementacion del AuthenticationProvider que se utiliza
+        comunmente para autenticar usuarios en BD. Además, es el respondable
+         de verificar las credenciales del usuario y autenticar el usuario*/
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    //Luego de lo de arriba tenemos que registrarlo en el manejador de autenticacion
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
+        AuthenticationManagerBuilder authMB = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        //cuando se realiza una solicitud el administrador de autenticacion usara este provider
+        authMB.authenticationProvider(authenticationProvider());
+        return authMB.build();
+    }
+
+    //CREAMOS un FILTRO (define las reglas de autorizacion para las solicitudes HTTP)
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(
---
+++ src/main/java/com/tcna/primeraweb/services/MyUserDetails.java
@@ -0,0 +1,34 @@
+package com.tcna.primeraweb.services;
+
+import com.tcna.primeraweb.models.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@AllArgsConstructor
+public class MyUserDetails implements UserDetails {
+
+    private User user;
+
+    //M3todo que nos obtiene los roles de un usuario
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+        return Arrays.asList(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+}
